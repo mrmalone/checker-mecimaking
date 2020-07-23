d54755b922d4785cae9bc11b230d4381d4bafa9b
Aioanei Alexandru <aioanei_alexandruSV@yahoo.com>
2020-07-23 22:24:49 +0300

    initial

---
+++ LoggerBeta/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace LoggerBeta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.launchbutton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statechecker = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Azure;
+            this.label1.Location = new System.Drawing.Point(272, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anti-Cheat V1.0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label2.Location = new System.Drawing.Point(12, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Developed by Mr.Malone";
+            // 
+            // launchbutton
+            // 
+            this.launchbutton.BackColor = System.Drawing.Color.Magenta;
+            this.launchbutton.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.launchbutton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.launchbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.launchbutton.Location = new System.Drawing.Point(277, 55);
+            this.launchbutton.Name = "launchbutton";
+            this.launchbutton.Size = new System.Drawing.Size(164, 28);
+            this.launchbutton.TabIndex = 3;
+            this.launchbutton.Text = "LAUNCH";
+            this.launchbutton.UseVisualStyleBackColor = false;
+            this.launchbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(383, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CURRENT STATE:";
+            // 
+            // statechecker
+            // 
+            this.statechecker.AutoSize = true;
+            this.statechecker.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statechecker.ForeColor = System.Drawing.Color.Snow;
+            this.statechecker.Location = new System.Drawing.Point(520, 139);
+            this.statechecker.Name = "statechecker";
+            this.statechecker.Size = new System.Drawing.Size(49, 24);
+            this.statechecker.TabIndex = 5;
+            this.statechecker.Text = "OFF";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LoggerBeta.Properties.Resources.health_shieldpink;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(581, 169);
+            this.Controls.Add(this.statechecker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.launchbutton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Checker MeciMaking";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button launchbutton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label statechecker;
+    }
+}
+
---
+++ LoggerBeta/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoggerBeta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            statechecker.ForeColor = System.Drawing.Color.Red;
+            LogThis("Aplicatia a fost lansata!");
+        }
+
+        // apps
+
+        static void LogThis(string logMessage)
+        {
+            Console.WriteLine(logMessage);
+            using (StreamWriter writer = new StreamWriter("logger.txt", true))
+            {
+                writer.WriteLine("[ " + FileDate() + " ] " + logMessage);
+                writer.WriteLine();
+            }
+        }
+
+        static string FileDate()
+        {
+            return DateTime.Now.ToString("yyyy") + "-" + DateTime.Now.ToString("MM") + "-" + DateTime.Now.ToString("dd") + " _ " + DateTime.Now.ToString("HH") + "-" + DateTime.Now.ToString("mm") + "-" + DateTime.Now.ToString("ss");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "Esti de acord cu monitorizarea activitatilor tale pe tot parcursul meciului?";
+            string title = "Warning!";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                this.WindowState = FormWindowState.Minimized;
+                statechecker.Text = "ON";
+                statechecker.ForeColor = System.Drawing.Color.GreenYellow;
+                launchbutton.Hide();
+                LogThis("Anti-Cheat rulat cu succes!");
+            }
+            else
+            {
+                MessageBox.Show("Anti-Cheatul nu a fost pus in sarcina!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                string message = "Aplicatia se va inchide. Esti sigur?";
+                message += "\n" + "Daca esti in continuare in meci sau pe un server securizat:";
+                message += "\n" + "* Vei fi exclus in mod instant de pe server";
+                message += "\n" + "* Vei primi o penalizare de 60 minute";
+                string title = "Avertisment";
+                DialogResult result = MessageBox.Show(message, title, MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    LogThis("Anti-Cheat a fost oprit de catre utilizator!");
+                    Application.Exit();
+                }
+                else if (result == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
